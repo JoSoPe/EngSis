380fc607cf7bc19a613eaeeaf408f942c71a95a6
JoSoPe <26833958+JoSoPe@users.noreply.github.com>
2018-05-13 21:03:52 +0200

    afegit atribut data_type a la data base

--- dades.py
+++ dades.py
@@ -4,7 +4,7 @@ import sqlite3 as lite
 import sys
 
 
-def read_txt():
+def read_txt(m):
  #
     con=lite.connect('lecturasensor.db')
     with con:    
@@ -15,11 +15,13 @@ def read_txt():
         rowid integer primary key,
         eixX real(5),
         eixY real(5),
-        eixZ real(5)
+        eixZ real(5),
+        data_type varchar(10)
         );
         """)
         con.commit()
-        #
+        
+        #prova_id integer(50)
     f = open("sensor.txt","r")
     info = f.readlines()
     read = [x.strip() for x in info]
@@ -27,36 +29,30 @@ def read_txt():
     for sample in read:
         t.append(tuple(sample.split(" , ")))
     data = list(t)
-    action = "INSERT INTO lectures(eixX,eixY,eixZ) VALUES (?,?,?)"
+    action = "INSERT INTO lectures(eixX,eixY,eixZ,data_type) VALUES (?,?,?,?)"
     con = lite.connect('lecturasensor.db')
+    print (data)
+    #print (m)
+    add = ()
+    add = m
+    #print (add)
+    t2 = []
+    for tupla in data:
+        tupla2 = tupla + (add,)
+        print (tupla2)
+        t2.append(tupla2)
+        
+    print (t2)
     with con:
         cur = con.cursor()
-        cur.executemany("INSERT INTO lectures(eixX,eixY,eixZ) VALUES (?,?,?)",data)
+        cur.executemany("INSERT INTO lectures(eixX,eixY,eixZ,data_type) VALUES (?,?,?,?)",t2)
         con.commit()
     con.close()
     f.close()
 
-def txt2database():
-    con=lite.connect('lecturasensor.db')
-    with con:
-        cur = con.cursor()
-        cur.executescript("""
-        DROP TABLE IF EXISTS lectures;
-        CREATE TABLE lectures(
-        rowid integer primary key,
-        eixX real(5),
-        eixY real(5),
-        eixZ real(5)
-        );
-        """)
-        con.commit()
-        read_txt()
-
-    con.close()
 
 def main():
     print ("main")
-    #txt2database()
-    read_txt()
-
+    m = str(input("entra tipus de dada:  "))
+    read_txt(m)
 main()
